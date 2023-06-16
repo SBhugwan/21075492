37c624fa42f5dbf29e708308dfdedc57a49d0af2
SBhugwan <sahil.bhugwan@gmail.com>
2023-06-16 22:36:07 +0200

    update

---
+++ Q5/code/Q5.R
@@ -0,0 +1,17 @@
+UR<-read.csv("/Users/sahilbhugwan/Downloads/DatsciPractical23/data/googleplay/googleplaystore_user_reviews.csv")
+
+GPS<- read_csv("/Users/sahilbhugwan/Downloads/DatsciPractical23/data/googleplay/googleplaystore.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
